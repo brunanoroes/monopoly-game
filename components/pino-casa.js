9a5bfb19dcc109c018170a308a554beb2e8b2e56
brunanoroes <143547466+brunanoroes@users.noreply.github.com>
2025-10-19 10:39:37 -0300

    feat: add new house images and update game logic

--- components/pino-casa.js
+++ components/pino-casa.js
@@ -9,6 +9,9 @@ Vue.component('pino-casa', {
   `,
   computed: {
     imagem() {
+      if (this.casaConstruida === 5) {
+        return 'assets/barracapraia.png';
+      }
       return `assets/casas/${this.proprietarioCor}/casa${this.casaConstruida}.png`;
     }
   }
