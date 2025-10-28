33099aeee5cb0662ff2eaf252b025d150dab91ba
brunanoroesuff <143547466+brunanoroes@users.noreply.github.com>
2025-10-28 11:28:20 -0300

    bloqueando modal de sorte para não bugar

--- models/SorteModel.js
+++ models/SorteModel.js
@@ -7,23 +7,26 @@ export default class Sorte extends Casa {
     }
     
     funcao(jogador, modal) {
-    const carta = this.cartasSorte.shift(); // pega a primeira carta
-    this.cartasSorte.push(carta); // coloca no final do baralho
+        modal.tipo = 2;
+        modal.mostra = true;
+        modal.mensagem = 'Ainda não implementado';
+    // const carta = this.cartasSorte.shift(); // pega a primeira carta
+    // this.cartasSorte.push(carta); // coloca no final do baralho
 
-    modal.tipo = 1;
-    modal.mostra = true;
-    modal.mensagem = carta.descricao || carta.conteudo;
+    // modal.tipo = 1;
+    // modal.mostra = true;
+    // modal.mensagem = carta.descricao || carta.conteudo;
 
-    // Exemplos de efeitos simples:
-    if (carta.conteudo.includes('ponto de partida')) {
-      jogador.localizacaoAtual = 0;
-      jogador.receber(200);
-    }
-    if (carta.conteudo.includes('Multa')) {
-      jogador.pagar(15);
-    }
-    if (carta.conteudo.includes('prisão')) {
-      jogador.localizacaoAtual = 10; // posição da prisão
-    }
+    // // Exemplos de efeitos simples:
+    // if (carta.conteudo.includes('ponto de partida')) {
+    //   jogador.localizacaoAtual = 0;
+    //   jogador.receber(200);
+    // }
+    // if (carta.conteudo.includes('Multa')) {
+    //   jogador.pagar(15);
+    // }
+    // if (carta.conteudo.includes('prisão')) {
+    //   jogador.localizacaoAtual = 10; // posição da prisão
+    // }
   }
 }
